ae6aa31d83cc8c79ec11f2c6201f6082055b33ff
qw1zz <dakalyakin_1@edu.hse.ru>
2024-04-24 02:57:52 +0300

    add output

---
+++ 4_5/a.out.c
@@ -0,0 +1,54 @@
+Philosopher 1 is thinking.
+Philosopher 2 is thinking.
+Philosopher 3 is thinking.
+Philosopher 4 is thinking.
+Philosopher 5 is thinking.
+Philosopher 2 wants to eat.
+Philosopher 3 wants to eat.
+Philosopher 3 is eating.
+Philosopher 5 wants to eat.
+Philosopher 4 wants to eat.
+Philosopher 1 wants to eat.
+Philosopher 1 is eating.
+Philosopher 1 ends eating.
+Philosopher 1 is thinking.
+Philosopher 3 ends eating.
+Philosopher 3 is thinking.
+Philosopher 5 is eating.
+Philosopher 2 is eating.
+Philosopher 1 wants to eat.
+Philosopher 3 wants to eat.
+Philosopher 2 ends eating.
+Philosopher 2 is thinking.
+Philosopher 5 ends eating.
+Philosopher 5 is thinking.
+Philosopher 4 is eating.
+Philosopher 1 is eating.
+Philosopher 2 wants to eat.
+Philosopher 5 wants to eat.
+Philosopher 1 ends eating.
+Philosopher 1 is thinking.
+Philosopher 4 ends eating.
+Philosopher 4 is thinking.
+Philosopher 3 is eating.
+Philosopher 5 is eating.
+Philosopher 4 wants to eat.
+Philosopher 3 ends eating.
+Philosopher 5 ends eating.
+Philosopher 5 is thinking.
+Philosopher 2 is eating.
+Philosopher 4 is eating.
+Philosopher 3 is thinking.
+Philosopher 1 wants to eat.
+Philosopher 4 ends eating.
+Philosopher 4 is thinking.
+Philosopher 2 ends eating.
+Philosopher 2 is thinking.
+Philosopher 1 is eating.
+Philosopher 5 wants to eat.
+Philosopher 3 wants to eat.
+Philosopher 3 is eating.
+Philosopher 1 ends eating.
+Philosopher 5 is eating.
+Philosopher 1 is thinking.
+Ending program
